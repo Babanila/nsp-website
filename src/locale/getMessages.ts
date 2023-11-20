6887a451f79ab9098656b84495052756760df09c
Babajide Williams <babanila@yahoo.com>
2023-11-20 17:53:05 +0100

    Get messages from translation files

---
+++ src/locale/getMessages.ts
@@ -0,0 +1,13 @@
+import messagesEn from '../../locales/messages/en.json';
+import messagesDe from '../../locales/messages/de.json';
+
+export type Locale = 'en' | 'de';
+
+export const getMessages = (locale: Locale) => {
+    switch (locale) {
+        case 'de':
+            return { ...messagesDe };
+        default:
+            return { ...messagesEn };
+    }
+};
